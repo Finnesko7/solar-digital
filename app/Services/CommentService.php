492e2fac1683040d62dce8ea174b63d11917910c
finnesko <finnesko7@gmail.com>
2020-08-16 20:28:56 +0300

    fix set recursive comments

--- app/Services/CommentService.php
+++ app/Services/CommentService.php
@@ -44,7 +44,7 @@ class CommentService
             $this->setRecursive($list, $comment);
 
             if (isset($list[$comment['parent_id']]) && $comment) {
-                $list[$comment['parent_id']]['comments'] = $comment;
+                $list[$comment['parent_id']]['comments'][] = $comment;
             } else {
                 $list[$comment['id']] = $comment;
             }
@@ -54,22 +54,12 @@ class CommentService
 
     private function setRecursive(array &$comments, array &$data)
     {
-        if (isset($comments['id'])) {
-            if ($comments['id'] == $data['parent_id']) {
-                $comments['comments'] = $data;
+        foreach ($comments as $key => &$comment) {
+            if (isset($comment['id']) && $comment['id'] == $data['parent_id']) {
+                $comment['comments'][] = $data;
                 $data = null;
-            }
-        } else {
-            foreach ($comments as &$comment) {
-                if ($comment['id'] == $data['parent_id']) {
-                    $comment['comments'] = $data;
-                } elseif (isset($comment['comments']) && is_array($comment['comments'])) {
-                    $result = $this->setRecursive($comment['comments'], $data);
-
-                    if ($result) {
-                        break;
-                    }
-                }
+            } elseif (isset($comment['comments'])) {
+                $this->setRecursive($comment['comments'], $data);
             }
         }
     }
